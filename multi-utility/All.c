53a5d6a43dadc425a2daeb97fe798c1bc2b2aee5
___Gravity___ <111515970+Empty-Space-Gravity@users.noreply.github.com>
2025-07-25 17:02:57 +0530

    Add files via upload

---
+++ multi-utility/All.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+    // float c;
+    // scanf("%f",&c);
+    // float fahrenheit = (9/5 * c) + 32;
+    // printf("%f",fahrenheit);
+    // getch();
+      
+    
+    // float salary = 10000;
+    // float percent = 23;
+    // float result = (salary * percent) / 100;
+    // printf("23%% of %.2f is %.2f\n", salary, result);
+
+
+    // float all = salary + result;
+    // printf("%.2f", all);
+    // return 0;
+    
+ 
+    // int one = 12;
+    // int two;
+    // int third =90;
+    // int sum = 180;
+
+
+    // two = sum - one - third;
+    // printf("The second angle is: %d\n", two);
+
+
+
+    // int x =12;
+    // int y = 15;
+    
+    // int kuch = (x*x) +(2*x*y) + (y*y);
+    // printf("The value of kuch is: %d\n", kuch);
+
+    
+
+    // int x =12;
+    // int y = 15;
+    
+    // int kuch = (x*x) - (2*x*y) + (y*y);
+    // printf("The value of kuch is: %d\n", kuch);
+    
+    
+
+    // int x = 12;
+    // int y = 15;
+    
+    // int kuch = (x*x*x) - (3*x*x*y) + (3*x*y*y) - (y*y*y);
+
+    // printf("The value of kuch is: %d\n", kuch);
+
+
+
+
+}
+
+
